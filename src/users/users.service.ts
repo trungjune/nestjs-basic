cb9fe6dca6f0290261259c149ff4bfba477de60d
trungth <trungth@vnpt-technology.vn>
2024-05-22 17:30:06 +0700

    hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 import { Model } from 'mongoose';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
@@ -8,10 +9,18 @@ import { User } from './schemas/user.schema';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+
   async create(email: string, password: string, name: string) {
+    const hashPassword = this.getHashPassword(password);
+
     const user = await this.userModel.create({
       email,
-      password,
+      password: hashPassword,
       name,
     });
     return user;
